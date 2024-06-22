4b1c8c2aa6ebd3655fc92585aac7c20e149c784a
Maximilian Y.O <myo@mail.uni-paderborn.de>
2024-06-23 00:02:23 +0200

    Added LinkLeaveEventHandler to build Histogram for Links

--- src/main/java/org/matsim/analysis/SimpleAnalysis.java
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -3,8 +3,11 @@ package org.matsim.analysis;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 
+import java.util.Map;
+
 public class SimpleAnalysis {
     // What we want to do: Reaed through the output events file and capture all the person arrival and person departure events.
+
 
     public static void main(String[] args){
 
@@ -14,5 +17,10 @@ public class SimpleAnalysis {
         manager.addHandler(handler);
 
         EventsUtils.readEvents(manager, "./scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
+
+        for(Map.Entry entry : handler.linkHistogram.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        System.out.println();
     }
 }
--- src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
+++ src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
@@ -1,19 +1,45 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.LinkLeaveEvent;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
 
-public class SimplePersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+import java.util.HashMap;
+import java.util.Map;
 
-    @Override
-    public void handleEvent(PersonArrivalEvent event) {
-        System.out.println("Arrival: " + event.getTime() + " : " + event.getPersonId());
-    }
+import static java.lang.Math.floor;
+
+public class SimplePersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler, LinkLeaveEventHandler {
+
+    private final Map<Id<Person>, Double> personToDepartureTime = new HashMap<>();
+    public final Map<Double, Double> linkHistogram = new HashMap<>();
 
     @Override
     public void handleEvent(PersonDepartureEvent event) {
-        System.out.println("Departure: " + event.getTime() + " : " + event.getPersonId());
+        personToDepartureTime.put(event.getPersonId(), event.getTime());
+    }
+
+    @Override
+    public void handleEvent(PersonArrivalEvent event) {
+        //System.out.println("Person: " + event.getPersonId() + "Travel time: " + (event.getTime() - personToDepartureTime.get(event.getPersonId())) + "s");
+    }
+
+    @Override
+    public void handleEvent(LinkLeaveEvent event) {
+        if(!event.getLinkId().toString().equals("908198590000r")){
+            return;
+        }
+
+        if(linkHistogram.containsKey(floor(event.getTime()/3600))){
+            linkHistogram.put(floor(event.getTime()/3600), linkHistogram.get(floor(event.getTime()/3600))+1);
+        } else {
+            linkHistogram.put(floor(event.getTime()/3600), 1.0);
+        }
     }
 }
