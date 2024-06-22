f261880febc4edd22b3ab713f3fc3ceefd8ee7f5
Maximilian Y.O <myo@mail.uni-paderborn.de>
2024-06-22 14:51:25 +0200

    Added simple analysis containing files SimpleAnalysis and SimplePersonEventHandler classes

---
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -0,0 +1,18 @@
+package org.matsim.analysis;
+
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+
+public class SimpleAnalysis {
+    // What we want to do: Reaed through the output events file and capture all the person arrival and person departure events.
+
+    public static void main(String[] args){
+
+        SimplePersonEventHandler handler = new SimplePersonEventHandler();
+        // Manages all our EventHandlers (we can have multiple ones, in this case we only have one). Pushes the events from the file into the EventHandler
+        EventsManager manager = EventsUtils.createEventsManager();
+        manager.addHandler(handler);
+
+        EventsUtils.readEvents(manager, "./scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
+    }
+}
